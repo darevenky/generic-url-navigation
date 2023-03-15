4528683cd70209b71a6771519a8c5b4a51a8d86f
darevenky <darevenky96@gmail.com>
2023-03-16 00:01:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def venky(request):
+    return render(request,'venky.html')
+
+def venky1(request):
+    return render(request,'venky1.html')
